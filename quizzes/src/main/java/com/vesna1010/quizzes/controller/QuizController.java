b11e2a2d171b44efbcfe8f9a7b8188f90a5370ee
Vesna S <vesnasdoboj@gmail.com>
2020-08-09 04:27:17 +0200

    Update QuizController.java

--- quizzes/src/main/java/com/vesna1010/quizzes/controller/QuizController.java
+++ quizzes/src/main/java/com/vesna1010/quizzes/controller/QuizController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.vesna1010.quizzes.model.Quiz;
 import com.vesna1010.quizzes.service.QuizService;
 
-@RestController
+@Controller
 @RequestMapping("/quizzes")
 public class QuizController {
 
